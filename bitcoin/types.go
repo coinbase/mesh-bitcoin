acfe2006f58dff54c42735874fcd17b2e35d3537
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2020-12-02 22:43:21 -0800

    Update types.go

--- bitcoin/types.go
+++ bitcoin/types.go
@@ -85,6 +85,7 @@ const (
 	P2PKHScriptPubkeySize = 25               // P2PKH size
 )
 
+// override default mainnet settings with address prefixes
 func CreateMainNetParams() (*chaincfg.Params) {
 	params := &chaincfg.MainNetParams
 	params.PubKeyHashAddrID = 0
@@ -93,6 +94,7 @@ func CreateMainNetParams() (*chaincfg.Params) {
 	return params
 }
 
+// override default testnet settings with address prefixes
 func CreateTestNet3Params() (*chaincfg.Params) {
 	params := &chaincfg.TestNet3Params
 	params.PubKeyHashAddrID = 111
