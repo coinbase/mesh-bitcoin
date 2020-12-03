649ee8bd543bab120684812e87e6edd2caa0add6
jagdeep sidhu <jsidhu@blockchainfoundry.co>
2020-12-02 22:58:05 -0800

    fix testnet network comment

--- bitcoin/types.go
+++ bitcoin/types.go
@@ -94,7 +94,7 @@ func CreateMainNetParams() (*chaincfg.Params) {
 	return params
 }
 
-// CreateTestNet3Params is a function to override default mainnet settings with address prefixes
+// CreateTestNet3Params is a function to override default testnet settings with address prefixes
 func CreateTestNet3Params() (*chaincfg.Params) {
 	params := &chaincfg.TestNet3Params
 	params.PubKeyHashAddrID = 111
