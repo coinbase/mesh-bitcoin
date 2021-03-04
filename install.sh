a8153102b764297ed68c341a2facc8ffa6d6aba7
DeFiDev <defidev@cakedefi.com>
2021-03-04 10:20:36 +0300

    install.sh: rename name references from bitcoin to defichain

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ set -e
 usage() {
   this=$1
   cat <<EOF
-$this: download pre-compiled Docker images for coinbase/rosetta-bitcoin 
+$this: download pre-compiled Docker images for coinbase/rosetta-defichain 
 
 Usage: $this [-d]
   -d turns on debug logging
@@ -44,8 +44,8 @@ execute() {
   log_info "downloading image into ${tmpdir}"
   http_download "${tmpdir}/${TARBALL}" "${TARBALL_URL}" "" "1"
   docker load --input "${tmpdir}/${TARBALL}"
-  docker tag "rosetta-bitcoin:${TAG}" "rosetta-bitcoin:latest"
-  log_info "loaded rosetta-bitcoin:${TAG} and tagged as rosetta-bitcoin:latest"
+  docker tag "rosetta-defichain:${TAG}" "rosetta-defichain:latest"
+  log_info "loaded rosetta-defichain:${TAG} and tagged as rosetta-defichain:latest"
   rm -rf "${tmpdir}"
   log_info "removed temporary directory ${tmpdir}"
 }
@@ -196,10 +196,10 @@ End of functions from https://github.com/client9/shlib
 ------------------------------------------------------------------------
 EOF
 
-BINARY=rosetta-bitcoin
+BINARY=rosetta-defichain
 FORMAT=tar.gz
 OWNER=coinbase
-REPO="rosetta-bitcoin"
+REPO="rosetta-defichain"
 PREFIX="$OWNER/$REPO"
 
 # use in logging routines
